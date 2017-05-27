a50ff00bedf4f36ae417e452c551c440cc7566b6
mbrsalman5 <salman@sqindia.net>
2017-05-27 15:51:19 +0530

    Agent Commit

--- app/src/main/java/net/sqindia/movhaulagent/CompanyFragment.java
+++ app/src/main/java/net/sqindia/movhaulagent/CompanyFragment.java
@@ -22,64 +22,24 @@ import com.sloop.fonts.FontsManager;
 
 public class CompanyFragment extends Fragment {
 
-    TextView tv_header_txt,tv_register,tv_admin;
-    TextInputLayout til_phone,til_user_name,til_password;
+    TextInputLayout til_phone;
     Typeface tf;
-    LinearLayout lt_agent,lt_admin;
     Button btn_submit;
+    TextView tv_activity_header;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View get_LoginView = inflater.inflate(R.layout.company_fragment, container, false);
+        View get_CompanyView = inflater.inflate(R.layout.company_fragment, container, false);
         FontsManager.initFormAssets(getActivity(), "fonts/lato.ttf");
-        FontsManager.changeFonts(get_LoginView);
+        FontsManager.changeFonts(get_CompanyView);
         tf = Typeface.createFromAsset(getActivity().getAssets(), "fonts/lato.ttf");
 
-
-        tv_header_txt = (TextView)getActivity().findViewById(R.id.textview_header);
-
-        lt_agent = (LinearLayout) get_LoginView.findViewById(R.id.agent_layout);
-        lt_admin = (LinearLayout) get_LoginView.findViewById(R.id.admin_layout);
-        til_phone = (TextInputLayout) get_LoginView.findViewById(R.id.textinput_number);
-        til_user_name = (TextInputLayout) get_LoginView.findViewById(R.id.textinput_username);
-        til_password = (TextInputLayout) get_LoginView.findViewById(R.id.textinput_password);
-        tv_register = (TextView) get_LoginView.findViewById(R.id.textview_register);
-        btn_submit = (Button) get_LoginView.findViewById(R.id.button_submit);
-
-        tv_admin = (TextView) get_LoginView.findViewById(R.id.textview_admin);
-
-
-        til_phone.setTypeface(tf);
-
-        tv_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.fragment_container, new RegisterFragment());
-                ft.commit();
-                tv_header_txt.setText(getString(R.string.register));
-            }
-        });
-
-        tv_admin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(tv_admin.getText().toString().equals("Change to Admin")){
-                    tv_admin.setText("Change to Agent");
-                    lt_agent.setVisibility(View.GONE);
-                    lt_admin.setVisibility(View.VISIBLE);
-                }
-                else{
-                    tv_admin.setText("Change to Admin");
-                    lt_agent.setVisibility(View.VISIBLE);
-                    lt_admin.setVisibility(View.GONE);
-                }
-            }
-        });
-
+        btn_submit = (Button) get_CompanyView.findViewById(R.id.button_submit);
+        tv_activity_header = (TextView)getActivity().findViewById(R.id.textview_header);
+        tv_activity_header.setText("Company");
 
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,7 +50,7 @@ public class CompanyFragment extends Fragment {
         });
 
 
-        return get_LoginView;
+        return get_CompanyView;
 
 
     }
--- app/src/main/java/net/sqindia/movhaulagent/Dashboard.java
+++ app/src/main/java/net/sqindia/movhaulagent/Dashboard.java
@@ -1,33 +1,23 @@
 package net.sqindia.movhaulagent;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.graphics.Typeface;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.sloop.fonts.FontsManager;
@@ -57,6 +47,8 @@ public class Dashboard extends AppCompatActivity{
     int sts;
     ListView lview_state,lview_district;
     FrameLayout fl_header,fl_bottom;
+    boolean bl_bottom;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,28 +76,6 @@ public class Dashboard extends AppCompatActivity{
             @Override
             public void onClick(View v) {
 
-   /*             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.replace(R.id.fragment_container, new RegisterFragment());
-                ft.commit();*/
-   //popup();
-
-                //popupwithpager();
-
-
-/*
-                Dialog_Region dialog_region = new Dialog_Region(Dashboard.this);
-                dialog_region.getWindow().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.white)));
-                dialog_region.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-                dialog_region.show();
-                //  getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
-
-                dialog_region.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-
-                    }
-                });
-*/
 
                 popupwithlistview();
 
@@ -118,105 +88,38 @@ public class Dashboard extends AppCompatActivity{
 
                 FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
                 ft.replace(R.id.fragment_container, new CompanyFragment());
+                ft.addToBackStack("driver");
                 ft.commit();
                 fl_bottom.setVisibility(View.GONE);
+                tv_header.setText("Company");
 
             }
         });
     }
 
-    public void popup(){
 
-
-
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(Dashboard.this);
-        LayoutInflater inflater = getLayoutInflater();
-        final View dialogView = inflater.inflate(R.layout.dialog_choose_driver, null);
-        dialogBuilder.setView(dialogView);
-        final AlertDialog b = dialogBuilder.create();
-
-        Spinner spinner = (Spinner) dialogView.findViewById(R.id.spinner);
-
-        ar_comp_lists.add("Choose Company");
-        ar_comp_lists.add("IBM");
-        ar_comp_lists.add("MovHaul");
-        ar_comp_lists.add("IfindCard");
-        ar_comp_lists.add("Opiniion");
-        ar_comp_lists.add("Sqindia");
-
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item,R.id.text1, ar_comp_lists);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(R.layout.spinner_item);
-
-        // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
-
-        b.show();
-
-//
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String item = parent.getItemAtPosition(position).toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        bl_bottom = fl_bottom.getVisibility()== View.VISIBLE ;
+        outState.putBoolean("bl_layout", bl_bottom);
     }
 
-  /*  public void popupwithpager(){
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(Dashboard.this);
-        LayoutInflater inflater = getLayoutInflater();
-        final View dialogView = inflater.inflate(R.layout.dialog_region, null);
-        dialogBuilder.setView(dialogView);
-         b = dialogBuilder.create();
-        tv_header = (TextView) dialogView.findViewById(R.id.textview_header);
-        viewPager = (ViewPager) dialogView.findViewById(R.id.view_pager);
-        img_back = (ImageView) dialogView.findViewById(R.id.img_back);
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        bl_bottom = savedInstanceState.getBoolean("bl_layout");
+        fl_bottom.setVisibility(bl_bottom ? View.VISIBLE : View.GONE);
+    }
 
-        layouts = new int[]{
-                R.layout.register_state,
-                R.layout.register_state };
-
-        state_lists.add("Private Driver");
-        state_lists.add("Company Driver");
-
-
-        district_lists.add("IBM");
-        district_lists.add("MovHaul");
-        district_lists.add("Opiniion");
-        district_lists.add("Sqindia");
-        district_lists.add("IFindCard");
-        district_lists.add("Zoho");
-
-        img_back.setVisibility(View.GONE);
-
-        myViewPagerAdapter = new MyViewPagerAdapter();
-        viewPager.setAdapter(myViewPagerAdapter);
-        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
-        viewPager.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return true;
-            }
-        });
-
-
-        b.show();
-
-
-
-
-    }*/
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        Log.e("tag","frag_count : "+getSupportFragmentManager().getBackStackEntryCount());
+        Log.e("tag","frag_countee : "+getSupportFragmentManager().findFragmentByTag("driver"));
+        Log.e("tag","frags : "+getSupportFragmentManager().getFragments());
+    }
 
     public void popupwithlistview(){
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(Dashboard.this);
@@ -269,6 +172,7 @@ public class Dashboard extends AppCompatActivity{
         Context cc;
         ArrayList<String> data_lists;
         int resourceid;
+        TextView tvview;
 
         public ListAdapter(Context context, int textViewResourceId, ArrayList<String> objects) {
             super(context, textViewResourceId, objects);
@@ -299,6 +203,7 @@ public class Dashboard extends AppCompatActivity{
             TextView label = (TextView) arow.findViewById(R.id.textview_header);
 
             final View vie_line = arow.findViewById(R.id.viewcolor);
+
 
             if(posi == data_lists.size()-1){
                 vie_line.setVisibility(View.GONE);
